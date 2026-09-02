581b91d49d6fc106ca602f6390c565287861f5c1
Abhinav Sengar <abhinavsengar.cs@gmailcom>
2026-09-02 12:55:12 +0530

    Initial Commit

---
+++ src/main/java/com/example/OAuth2_Basic/controllers/TestController.java
@@ -0,0 +1,25 @@
+package com.example.OAuth2_Basic.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class TestController {
+
+    @GetMapping("/public")
+    public String publicApi() {
+        return "Public API";
+    }
+
+    @GetMapping("/user")
+    public String userApi() {
+        return "User API";
+    }
+
+    @GetMapping("/admin")
+    public String adminApi() {
+        return "Admin API";
+    }
+}
---
+++ src/test/java/com/example/OAuth2_Basic/OAuth2BasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OAuth2_Basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuth2BasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
